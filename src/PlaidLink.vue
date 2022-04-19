e8897141c974fd1641ec784668e5a77c55d7d4c1
Braden Fineberg <bfine9618@gmail.com>
2022-04-19 13:02:43 -0500

    fix plaid for oauth

--- src/PlaidLink.vue
+++ src/PlaidLink.vue
@@ -25,13 +25,13 @@ export default {
     token: String,
     product: {
       type: [String, Array],
-      default: function () {
+      default: function() {
         return ["transactions"];
       },
     },
     receivedRedirectUri: {
       type: String,
-      default: null,
+      default: window.location.href,
     },
     language: String,
     countryCodes: Array,
@@ -87,7 +87,7 @@ export default {
       }
     },
     loadScript(src) {
-      return new Promise(function (resolve, reject) {
+      return new Promise(function(resolve, reject) {
         if (document.querySelector('script[src="' + src + '"]')) {
           resolve();
           return;
@@ -109,12 +109,18 @@ export default {
   },
   watch: {
     $props: {
-      handler(newVal) {
-        if (newVal.token) {
+      handler(newVal, oldVal) {
+        if (newVal.receivedRedirectUri) {
+          window.linkHandler.exit();
+          window.linkHandler.destroy();
+          this.onScriptLoaded();
+          window.linkHandler.open();
+        } else if (newVal.token) {
           this.onScriptLoaded();
         }
       },
       deep: true,
+      immediate: true,
     },
   },
 };
